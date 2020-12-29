6558eef07e83bbee0f6bc45b273ebb134ec356d7
Ashley Joseph <ashperce@gmail.com>
2020-12-28 19:44:12 -0500

    finished fronend challenge, added documentation and updated the read me, did extra: save nomination lists if the user leaves the page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,25 @@
+const operations = {
+    ADD: "add",
+    REMOVE: "remove",
+};
+// created using UUID to keep the name unique
+const storageKeys = {
+    value: "55a7769c-4969-11eb-b378-0242ac130002",
+}
+
+var NOMINEELIST = [];
+/**
+ * Check if there are nominees in the local storage, also add the event listener on the check box
+ */
 function setUpSearch() {
     hideBoxes(true);
+    if (localStorage.getItem(storageKeys.value) !== undefined) {
+        let nominees = JSON.parse(localStorage.getItem(storageKeys.value));
+        console.log(nominees)
+        for (let i = 0; i < nominees.length; i++) {
+            addNominee(nominees[i]);
+        }
+    }
     let searchBox = document.getElementById("searchBox");
     searchBox.addEventListener("keyup", () => {
         let searchWord = searchBox.value;
@@ -11,6 +31,12 @@ function setUpSearch() {
     });
 }
 
+/**
+ * helper method to create html attributes easily
+ * @param {string} elementName the name of the htlm element
+ * @param {object} attributes the different attributes of the html element ex class, style etc.
+ * @param {string} nodeText the inner html text if the element can display text
+ */
 function createElementHelper(elementName, attributes, nodeText) {
     nodeText = nodeText || "";
     attributes = attributes || [];
@@ -26,11 +52,10 @@ function createElementHelper(elementName, attributes, nodeText) {
     }
     return newElement;
 }
-const operations = {
-    ADD: "add",
-    REMOVE: "remove",
-};
-
+/**
+ * To fetch movie list from omdapi with the word written by the user
+ * @param {string} word the word to send the api call with
+ */
 function handleSearch(word) {
     hideBoxes(false);
     let searchWord = document.getElementById("searchWord");
@@ -46,9 +71,10 @@ function handleSearch(word) {
             }
         });
 }
-
-var nominees = [];
-
+/**
+ * To create the html elements on the page
+ * @param {Array} searchResult the parsed result array received from an api call
+ */
 function showResultCards(searchResult) {
     removeResultCards();
     const cardGroup = document.getElementById("cardGroup");
@@ -59,20 +85,16 @@ function showResultCards(searchResult) {
             "Nominate",
             operations.ADD
         );
-
         cardGroup.appendChild(cardElement);
-        if (nominees.find((e) => e.imdbID === searchResult[i].imdbID)) {
+        if (NOMINEELIST.find((e) => e.imdbID === searchResult[i].imdbID)) {
             document.getElementById(
                 searchResult[i].imdbID + operations.ADD
             ).disabled = true;
         }
-
-        // Some logic here after
         document
             .getElementById(searchResult[i].imdbID + operations.ADD)
             .addEventListener("click", () => {
-                if (nominees.length < 5 && !nominees.includes(searchResult[i])) {
-                    nominees.push(searchResult[i]);
+                if (NOMINEELIST.length < 5 && !NOMINEELIST.includes(searchResult[i])) {
                     addNominee(searchResult[i]);
                     document.getElementById(
                         searchResult[i].imdbID + operations.ADD
@@ -80,9 +102,9 @@ function showResultCards(searchResult) {
                     makeAlert(
                         "alert-success",
                         "You have " +
-                        nominees.length +
+                        NOMINEELIST.length +
                         " nomination(s). " +
-                        (5 - nominees.length) +
+                        (5 - NOMINEELIST.length) +
                         " remaining."
                     );
                 } else {
@@ -91,8 +113,13 @@ function showResultCards(searchResult) {
             });
     }
 }
-
+/**
+ * to parse and create nominee cards
+ * @param {Object} movieObj the movie object received from an api call
+ */
 function addNominee(movieObj) {
+    NOMINEELIST.push(movieObj);
+    localStorage.setItem(storageKeys.value, JSON.stringify(NOMINEELIST));
     let newCardElem = createCardElem(
         movieObj,
         "btn-danger",
@@ -102,7 +129,8 @@ function addNominee(movieObj) {
     let parent = document.getElementById("nomGroup");
     parent.appendChild(newCardElem);
     newCardElem.addEventListener("click", () => {
-        nominees = nominees.filter((e) => movieObj.imdbID !== e.imdbID);
+        NOMINEELIST = NOMINEELIST.filter((e) => movieObj.imdbID !== e.imdbID);
+        localStorage.setItem(storageKeys.value, JSON.stringify(NOMINEELIST));
         parent.removeChild(newCardElem);
         if (document.getElementById(movieObj.imdbID + operations.ADD) !== null) {
             document.getElementById(
@@ -111,7 +139,13 @@ function addNominee(movieObj) {
         }
     });
 }
-
+/**
+ * 
+ * @param {Object} search the movie object element
+ * @param {string} buttonClass the bootstrap class type of the button
+ * @param {string} buttonText the text displayed on the button
+ * @param {string} opp from enum operation, represent either ADD or DELETE
+ */
 function createCardElem(search, buttonClass, buttonText, opp) {
     let cardElem = createElementHelper("div", {
         class: "card text-justify",
@@ -164,11 +198,17 @@ function createCardElem(search, buttonClass, buttonText, opp) {
     cardElem.appendChild(cardBodyElem);
     return cardElem;
 }
-
+/**
+ * To clear the search result area
+ */
 function removeResultCards() {
     document.getElementById("cardGroup").innerHTML = "";
 }
-
+/**
+ * to dynamically creatte bootstrap alerts
+ * @param {string} htmlClass the bootstrap class of the alert
+ * @param {string} message the message to be displayed
+ */
 function makeAlert(htmlClass, message) {
     let alertElem = document.getElementById("messageAlert");
     alertElem.classList.add(htmlClass);
@@ -178,10 +218,13 @@ function makeAlert(htmlClass, message) {
         alertElem.innerText = "";
     }, 500);
 }
-
+/**
+ * to hide and show different boxes on the page
+ * @param {boolean} value true if boxes have to be hidden
+ */
 function hideBoxes(value) {
     let resultBox = document.getElementById("resultBox");
     resultBox.hidden = value;
-    let nominationBox = document.getElementById("nominationBox");
-    nominationBox.hidden = value;
+    // let nominationBox = document.getElementById("nominationBox");
+    // nominationBox.hidden = value;
 }
